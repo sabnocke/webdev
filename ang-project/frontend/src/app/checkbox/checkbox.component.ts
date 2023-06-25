53a7988b1d278ceeceee3ffe25de3d3b9e778110
sabnocke <hloch.michal@gmail.com>
2023-06-25 22:49:00 +0200

    update

---
+++ ang-project/frontend/src/app/checkbox/checkbox.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-checkbox',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './checkbox.component.html',
+  styleUrls: ['./checkbox.component.sass'],
+})
+export class CheckboxComponent {
+  @Input() name!: string;
+}
--- ang-project/frontend/src/app/modal/modal.component.ts
+++ ang-project/frontend/src/app/modal/modal.component.ts
@@ -11,6 +11,8 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
+import { MatIconModule } from '@angular/material/icon';
+import { CheckboxComponent } from '../checkbox/checkbox.component';
 import {
   FormGroup,
   FormControl,
@@ -29,11 +31,15 @@ import {
     MatDatepickerModule,
     MatNativeDateModule,
     FormsModule,
+    MatIconModule,
+    CheckboxComponent,
   ],
   templateUrl: './modal.component.html',
   styleUrls: ['./modal.component.sass'],
 })
 export class ModalComponent {
+  naming: string = 'Name Surname';
+
   range = new FormGroup({
     start: new FormControl<Date | null>(null),
     end: new FormControl<Date | null>(null),
@@ -49,6 +55,50 @@ export class ModalComponent {
     ) as HTMLDialogElement;
     this.cdr.detectChanges();
     this.dialogService.setDialog(dialog);
+
+    const nextButton = document.querySelector('.next-button');
+    const prevButton = document.querySelector('#prev-button');
+    const basic = document.getElementById('basic');
+    const _switch = document.getElementById('switch');
+
+    if (nextButton && basic && _switch) {
+      nextButton.addEventListener('click', () => {
+        basic.style.display = 'none';
+        _switch.style.display = 'inline-block';
+        console.log('clicked');
+      });
+    }
+    if (prevButton && basic && _switch) {
+      prevButton.addEventListener('click', () => {
+        basic.style.display = 'block';
+        _switch.style.display = 'none';
+        console.log('clicked');
+      });
+    }
+    const d = new Date();
+    const dateControl = document.getElementById('dpr') as HTMLInputElement;
+    const idl = document.getElementById('idl') as HTMLInputElement;
+    const pattern = '/(?::d{2}.[dw]+)/g';
+
+    const isoDate = (): string => {
+      const year = d.getFullYear();
+      const month = d.getMonth() + 1;
+      const day = d.getDate();
+      const hour = d.getHours();
+      const minute = d.getMinutes();
+      const dateString = `${year}-${month}-${day}T${hour}:${minute}`;
+      return dateString;
+    };
+    if (dateControl) {
+      //alert(`date: ${isoDate()}`);
+      dateControl.value = d.toISOString();
+      //alert(dateControl.value);
+    } else {
+      alert('problem');
+    }
+    if (idl) {
+      idl.value = isoDate();
+    }
   }
   openDialog(): void {
     this.dialogService.openDialog();
